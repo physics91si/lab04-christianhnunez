42bf74f63bd4b4e443b03383f6bfaa83d4ac73d3
Christian Nunez <chnunez@stanford.edu>
2018-04-17 00:20:45 -0700

    Lab04 finished :)

--- fib.py
+++ fib.py
@@ -30,6 +30,8 @@ Usage
         phi_approx(int(args[1]))
     elif args[0] == "converge" and len(args) == 1:
         phi_converge()
+    elif args[0] == "converge" and len(args) == 2:
+        phi_converge(args[1]);
     else:
         print "Error: input not understood.\n" \
                 "    Type './fib.py help' for info on this program."
@@ -53,13 +55,15 @@ phi_approx_output_format = \
     fib_(n-1): {:g}
     phi: {:.25f}"""
 
-def phi_approx(n, show_output=True):
+def phi_approx(n, show_output=True, stream=None):
     """Return the nth-order Fibonacci approximation to the golden ratio."""
     fib_n = fib(n)
     fib_nm1 = fib(n - 1)
     phi = float(fib_n)/fib_nm1
     if show_output:
         print phi_approx_output_format.format(n, fib_n, fib_nm1, phi)
+    if stream != None and show_output==True:
+        stream.write(phi_approx_output_format.format(n, fib_n, fib_nm1, phi))
     return phi
 
 phi_converge_output_format = \
@@ -67,18 +71,29 @@ phi_converge_output_format = \
     phi_old: {:.25f}
     phi_new: {:.25f}"""
 
-def phi_converge():
+def phi_converge(filename=""):
     """Keep calculating higher-order Fibonacci approximations to the golden
     ratio until it stops changing (to floating-point precision)."""
+    writeToFile = filename != ""
+    
+    if(writeToFile):
+        f = open(filename, "w")
 
     i = 3
-    phi_old = phi_approx(i - 1, show_output=False)
-    phi_new = phi_approx(i)
+    phi_old = phi_approx(i - 1, show_output=False, stream=f)
+    phi_new = phi_approx(i, show_output=True, stream=f)
     while phi_old != phi_new:
         i += 1
         phi_old = phi_new
         phi_new = phi_approx(i, show_output=False)
-        print phi_converge_output_format.format(i, phi_new, phi_old)
-    print "\nConverged to %.25f" % phi_new
+        if(writeToFile):
+            f.write(phi_converge_output_format.format(i, phi_new, phi_old))
+        else:
+            print phi_converge_output_format.format(i, phi_new, phi_old)
+    if(writeToFile):
+        f.write("\nConverged to %.25f" % phi_new)
+    else:
+        print "\nConverged to %.25f" % phi_new
+    f.close()
 
 if __name__ == '__main__': main()
